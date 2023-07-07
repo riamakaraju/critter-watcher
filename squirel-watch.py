046ed8d0d5d2a2ba24a9654becfec7e183186fb5
rai <riamakaraju@gmail.com>
2023-07-07 16:50:05 -0400

    second commit

--- squirel-watch.py
+++ squirel-watch.py
@@ -32,20 +32,22 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("filename", type=str, help="filename of the image to process")
 parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect.")
-
 args = parser.parse_args()
 
 # load an image (into shared CPU/GPU memory)
 img = loadImage(args.filename)
-
 # load the recognition network
 net = imageNet(args.network)
-
 # classify the image
 class_idx, confidence = net.Classify(img)
-
 # find the object description
 class_desc = net.GetClassDesc(class_idx)
 
+squirrel_counter = 0
+
 # print out the result
-#print("image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence * 100))
+if (class_desc == "fox squirrel, eastern fox squirrel, Sciurus niger"):
+    print("Squirrel alert! (: We are " + confidence*100 + " sure of it...")
+    squirrel_counter+=1
+    print("You have seen " + squirrel_counter + " squirrels.")
+
